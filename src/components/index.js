708524ce9c4fc1b7b3ee2e8ffcd54d513d55a4a9
Roy Ronalds <roy.ronalds@ellucian.com>
2018-08-30 17:21:57 -0400

    Add sample component to the package.

---
+++ src/components/index.js
@@ -0,0 +1,1 @@
+export { Shuriken } from './Shuriken'
---
+++ tests/NWSets.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Shuriken from '../components'
+
+describe('Overall Acceptance Testing', () => {
+	it('renders a shuriken component without crashing', () => {
+	  const div = document.createElement('div');
+	  ReactDOM.render(<Shuriken />, div);
+	  ReactDOM.unmountComponentAtNode(div);
+	});
+});
