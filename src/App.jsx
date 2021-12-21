5290ad461a339eae4e41e55027d5c1830d0b8afd
Roy R <tchalvakspam@gmail.com>
2021-12-20 23:08:13 -0500

    Minor change to import chain.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import React, { Component } from 'react'
-import logo from './logo.svg'
 import './App.css'
-import Shuriken from './components/Shuriken/Shuriken'
+
+import React, { Component } from 'react'
+
+import Shuriken from './components/Shuriken'
+import logo from './logo.svg'
 
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
